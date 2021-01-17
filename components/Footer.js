0caa21e91bbc6d9a776a03384641795b9b326220
Fabio Quarantini <fabio.quarantini@evoluzionetelematica.it>
2021-01-17 19:34:07 +0100

    Remove background

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 const Footer = () => (
-  <footer className="bg-white">
+  <footer>
     <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
       <div className="flex justify-center space-x-6 md:order-2">
         <a href="#" className="text-gray-400 hover:text-gray-500">
