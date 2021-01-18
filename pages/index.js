3aca188d17d86b0a574a4952b200825119d374a3
Fabio Quarantini <fabio.quarantini@evoluzionetelematica.it>
2021-01-18 09:31:37 +0100

    Update home template

--- pages/index.js
+++ pages/index.js
@@ -43,9 +43,13 @@ export default function Home() {
           </SwiperSlide>
         </Swiper>
       <Layout>
-        <h1 className="text-5xl text-center text-gray-700 dark:text-gray-100">
-          Basement
-        </h1>
+          <div class="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+            <div class="text-center">
+              <h2 class="text-base font-semibold text-indigo-600 tracking-wide uppercase">Basement</h2>
+              <p class="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">Take control of your project.</p>
+              <p class="max-w-xl mt-5 mx-auto text-xl text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lacinia, quam a ultrices malesuada.</p>
+            </div>
+          </div>
       </Layout>
       <Footer/>
     </>
